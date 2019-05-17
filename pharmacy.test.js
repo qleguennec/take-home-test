30c45c124c8ef7b8412d8d4b2c5c4727026bec40
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2019-05-17 19:15:43 +0200

    Revert "Unsure future commits correctness by testing against validated data"
    This reverts commit 6fede4c4942e28fed119fef1e615dfc47f6179ba.

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,17 +1,7 @@
 import { Drug, Pharmacy } from "./pharmacy";
-import testCases from "./output";
 
 describe("Pharmacy", () => {
   describe("updateBenefitValue()", () => {
-    testCases.forEach((testCase, index) => {
-      if (testCases[index + 1])
-        it(`should match given data on iteration ${index + 1}`, () => {
-          expect(new Pharmacy(testCase).updateBenefitValue()).toEqual(
-            testCases[index + 1]
-          );
-        });
-    });
-
     it("should return an empty list when passed an empty list", () => {
       expect(new Pharmacy([]).updateBenefitValue()).toEqual([]);
     });

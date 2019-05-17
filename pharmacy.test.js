5165858108bcad5022b63ce4316f8ecc70cbc4e0
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2019-05-17 17:17:30 +0200

    Unsure future commits correctness by testing against validated data
    This adds an output.js file based on output.txt.
    pharmacy.test.js uses it as a kind of snapshot to test against.
    This commit is meant to be reverted, as it has no real use upstream.

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,21 @@
 import { Drug, Pharmacy } from "./pharmacy";
+import testCases from "./output";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("updateBenefitValue()", () => {
+    testCases.forEach((testCase, index) => {
+      if (testCases[index + 1])
+        it(`should match given data on iteration ${index + 1}`, () => {
+          expect(new Pharmacy(testCase).updateBenefitValue()).toEqual(
+            testCases[index + 1]
+          );
+        });
+    });
+
+    it("should decrease the benefit and expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
   });
 });

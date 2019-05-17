fbd7ca4231cd45a8269198da82ba5d7dcd54e067
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2019-05-17 18:10:51 +0200

    Add specific tests for Herbal Tea

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -65,5 +65,42 @@ describe("Pharmacy", () => {
         new Pharmacy([new Drug("test", 0, 50)]).updateBenefitValue()[0].benefit
       ).toBeLessThanOrEqual(50);
     });
+
+    describe("Herbal Tea", () => {
+      it("should increment the benefit", () => {
+        expect(
+          new Pharmacy([new Drug("Herbal Tea", 2, 5)]).updateBenefitValue()[0]
+            .benefit
+        ).toBe(6);
+      });
+
+      it("should increment the benefit by two units when the expiresIn is equal to 0", () => {
+        expect(
+          new Pharmacy([new Drug("Herbal Tea", 0, 5)]).updateBenefitValue()[0]
+            .benefit
+        ).toBe(7);
+      });
+
+      it("should increment the benefit by two units when the expiresIn is less than 0", () => {
+        expect(
+          new Pharmacy([new Drug("Herbal Tea", -1, 5)]).updateBenefitValue()[0]
+            .benefit
+        ).toBe(7);
+      });
+
+      it("should not increment the benefit to a value greater than 50", () => {
+        expect(
+          new Pharmacy([new Drug("Herbal Tea", 0, 50)]).updateBenefitValue()[0]
+            .benefit
+        ).toBeLessThanOrEqual(50);
+      });
+
+      it("should decrement the expiresIn", () => {
+        expect(
+          new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()[0]
+            .expiresIn
+        ).toEqual(1);
+      });
+    });
   });
 });
